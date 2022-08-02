96c83168c77186b2cd7349838abfe2843f40b4c6
Abhishek Sharma <abhishek1sharma14.1998@gmail.com>
2022-08-02 20:07:04 +0530

    Routing Added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import "./App.css";
 import Header from "./components/Header";
 import Signin from "./components/Signin";
 import Signup from "./components/Signup";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Signup />
-      <Signin />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
-      <div className="header-logo">Logo</div>
+      <div className="header-logo">
+        <Link to="/">Logo</Link>
+      </div>
       <ul className="header-list">
         {/* <li>Logo</li> */}
-        <li>Home</li>
-        <li>Profile</li>
-        <li>Add Post</li>
-        <li>Login</li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/signin">
+          <li>SignIn</li>
+        </Link>
+        <Link to="/signup">
+          <li>SignUp</li>
+        </Link>
       </ul>
     </div>
   );
